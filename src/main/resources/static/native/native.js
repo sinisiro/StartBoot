4cb3be0db4335ff9aa384c167e2eec6da0e2c2e8
sinisiro <sinisiro@naver.com>
2022-07-05 09:05:40 +0900

    heroku Test

--- src/main/resources/static/native/native.js
+++ src/main/resources/static/native/native.js
@@ -39,7 +39,7 @@ $(document).ready(function(){
         $("#form01").attr("action","/native");
           form01.submit();
     });
-
+    //popup 테스트
     $("#btnPopup").click(function(){
         window.open("/native/appCall")
     });
@@ -58,10 +58,38 @@ $(document).ready(function(){
 
     });
 
+//    $("#btnBioMove").click(function(){
+//        $("#form01").attr("action","/native/appCall");
+//          form01.submit();
+//    });
+
     $("#btnBioMove").click(function(){
-        $("#form01").attr("action","/native/appCall");
-          form01.submit();
-    });
+
+            console.log("앱 스키마 호출");
+
+            var plan_no = "108018100520017";
+            var cust_no ="1";
+            var age= "40";
+            var sex = "1";
+
+
+            var url = "";
+            url += "dongbuins://aerox";
+            url += "?serviceID= "+ plan_no;
+            url += "&custNo ="+ cust_no;
+            url += "&serverPublicKeyVer =";
+            url += "&serverPublicKey =";
+            url += "&ins_plhd_dvn =";
+            url += "&ment ="
+            url += "&userAge =" + age;
+            url += "&userSex ="+sex;
+            url += "&BIO_SIG_TOKEN_NO = testToken";
+
+            setTimeout(function(){
+                location.href = url;
+            },0);
+        });
+
 
 
 
