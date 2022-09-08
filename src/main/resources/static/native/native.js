f5328cdc6a1b0903a158745b939f236540b7e2d6
sinisiro <sinisiro@naver.com>
2022-09-08 09:23:46 +0900

    커밋테스트

--- src/main/resources/static/native/native.js
+++ src/main/resources/static/native/native.js
@@ -66,7 +66,7 @@ $(document).ready(function(){
 
     $("#btnBioMove").click(function(){
 
-            console.log("앱 스키마 호출");
+            console.log("앱 스키마 호출!!!!");
 
             var plan_no = "108018100520017";
             var cust_no ="1";
