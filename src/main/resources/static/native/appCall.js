af709dc30d83aa01776dc4ff37a9342d73d43501
sinisiro <sinisiro@naver.com>
2022-10-07 17:25:07 +0900

    앱배로호출추가

--- src/main/resources/static/native/appCall.js
+++ src/main/resources/static/native/appCall.js
@@ -4,9 +4,11 @@
 */
 $(document).ready(function(){
 
-     $("#btnBio").click(function(){
 
-            console.log("앱 스키마 호출");
+    ("#btnBio").click(function(){
+
+            alert("호출");
+            console.log("앱 스키마 호출!!!!");
 
             var plan_no = "108018100520017";
             var cust_no ="1";
@@ -14,31 +16,64 @@ $(document).ready(function(){
             var sex = "1";
 
 
+
             var url = "";
-            url += "dongbuins://aerox";
-            url += "?serviceID= "+ plan_no;
-            url += "&custNo ="+ cust_no;
-            url += "&serverPublicKeyVer =";
-            url += "&serverPublicKey =";
-            url += "&ins_plhd_dvn =";
+            url += "dongbusign://aerox";
+            url += "?serviceID="+ plan_no;
+            url += "&custNo="+ cust_no;
+            url += "&serverPublicKeyVer=";
+            url += "&serverPublicKey=";
+            url += "&ins_plhd_dvn=";
+            url += "&dvcd=2";
             url += "&ment ="
-            url += "&userAge =" + age;
-            url += "&userSex ="+sex;
-            url += "&BIO_SIG_TOKEN_NO = testToken";
+            url += "&userAge=" + age;
+            url += "&userSex="+sex;
+            url += "&BIO_SIG_TOKEN_NO=testTokens";
+
+
 
             setTimeout(function(){
             //22.07 iOS 카카오 인앱에서 외부 앱 호출하면 인앱닫히는 현상 방지하기위함.
             $("#btn_appCall").attr("href", url)[0].click();
                 location.href = url;
             },0);
-
-//            setTimeout(function(){
-//                location.href = url;
-////                $("#btn_appcall").attr("href",url)[0].click();
-//            },0);
-
-
         });
+//
+//     $("#btnBio").click(function(){
+//
+//            console.log("앱 스키마 호출");
+//
+//            var plan_no = "108018100520017";
+//            var cust_no ="1";
+//            var age= "40";
+//            var sex = "1";
+//
+//
+//            var url = "";
+//            url += "dongbuins://aerox";
+//            url += "?serviceID= "+ plan_no;
+//            url += "&custNo ="+ cust_no;
+//            url += "&serverPublicKeyVer =";
+//            url += "&serverPublicKey =";
+//            url += "&ins_plhd_dvn =";
+//            url += "&ment ="
+//            url += "&userAge =" + age;
+//            url += "&userSex ="+sex;
+//            url += "&BIO_SIG_TOKEN_NO = testToken";
+//
+//            setTimeout(function(){
+//            //22.07 iOS 카카오 인앱에서 외부 앱 호출하면 인앱닫히는 현상 방지하기위함.
+//            $("#btn_appCall").attr("href", url)[0].click();
+//                location.href = url;
+//            },0);
+//
+////            setTimeout(function(){
+////                location.href = url;
+//////                $("#btn_appcall").attr("href",url)[0].click();
+////            },0);
+//
+//
+//        });
 
     $("#btnClose").click(function(){
         window.close();
