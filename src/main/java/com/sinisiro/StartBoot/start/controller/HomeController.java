93f1b52da0691ebd03b71bdab32ee68a1da1f075
sinisiro <sinisiro@naver.com>
2022-10-25 11:22:56 +0900

    universal Link 테스트

--- src/main/java/com/sinisiro/StartBoot/start/controller/HomeController.java
+++ src/main/java/com/sinisiro/StartBoot/start/controller/HomeController.java
@@ -1,17 +1,20 @@
 package com.sinisiro.StartBoot.start.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Controller
 @Slf4j
 public class HomeController {
 //    private static final Logger logger = LogManager.getLogger(HomeController.class);
-
+    private ObjectMapper objectMapper = new ObjectMapper();
 
     // 기본 페이지 설정되어어 있으면 이쪽을 index.html에 우선한다.
     @GetMapping("/")
@@ -31,12 +34,12 @@ public class HomeController {
 
     //https://ohgyun.com/707 참고함.
     @RequestMapping("/.well-known/apple-app-site-association")
-    public @ResponseBody String assa(HttpServletRequest req, HttpServletResponse res) {
+    public @ResponseBody void assa(HttpServletRequest req, HttpServletResponse res) throws IOException {
 
         res.setContentType("application/json");
-        res.setCharacterEncoding("UTF-8");
+        res.setCharacterEncoding("utf-8");
 
-        return "{\n" +
+        String assa =  "{\n" +
                 "    \"applinks\": {\n" +
                 "        \"details\": [\n" +
                 "            {\n" +
@@ -48,5 +51,8 @@ public class HomeController {
                 "        ]\n" +
                 "    }\n" +
                 "}";
+
+        String result = objectMapper.writeValueAsString(assa);
+        res.getWriter().write(result);
     }
 }
