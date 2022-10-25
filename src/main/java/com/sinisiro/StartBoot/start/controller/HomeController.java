b3a9689b7a2bdb1b91dd636dfda7f9994ce73281
sinisiro <sinisiro@naver.com>
2022-10-25 13:07:35 +0900

    universal Link 테스트

--- src/main/java/com/sinisiro/StartBoot/start/controller/HomeController.java
+++ src/main/java/com/sinisiro/StartBoot/start/controller/HomeController.java
@@ -3,6 +3,9 @@ package com.sinisiro.StartBoot.start.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -33,13 +36,16 @@ public class HomeController {
     }
 
     //https://ohgyun.com/707 참고함.
+    //ASSA를 루트나 .well-known 실물파일이 아닌 요청시 json으로 리턴하게끔  처리함.
+    //jsonParser: string->json 변환
     @RequestMapping("/.well-known/apple-app-site-association")
-    public @ResponseBody void assa(HttpServletRequest req, HttpServletResponse res) throws IOException {
+    public @ResponseBody void assa(HttpServletRequest req, HttpServletResponse res) throws IOException, ParseException {
 
         res.setContentType("application/json");
         res.setCharacterEncoding("utf-8");
 
-        String assa =  "{\n" +
+
+        String assa = "{\n" +
                 "    \"applinks\": {\n" +
                 "        \"details\": [\n" +
                 "            {\n" +
@@ -50,9 +56,14 @@ public class HomeController {
                 "            }\n" +
                 "        ]\n" +
                 "    }\n" +
-                "}";
+                "}\n";
 
-        String result = objectMapper.writeValueAsString(assa);
-        res.getWriter().write(result);
+        String str = "{\"name\" : \"apple\", \"id\" : 1, \"price\" : 1000}";
+
+        JSONParser parser = new JSONParser();
+        JSONObject jsonObject = (JSONObject) parser.parse(assa);
+
+
+        res.getWriter().write(jsonObject.toJSONString());
     }
 }
